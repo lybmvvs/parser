c7dae10a3ac202371c924c36ddc457806bb0985f
stik9 <stik9543@gmail.com>
2024-10-07 17:21:42 +0300

    feat(tests): test

--- tests/test.py
+++ tests/test.py
@@ -42,5 +42,6 @@ class TestLineParsersUnit:
              np.nan, np.nan]]
 
     def test_parser_result(self, test_parser_data):
+        print('sth')
 
         assert to_pars(self.input_file).values.tolist() == self.parsing_result
